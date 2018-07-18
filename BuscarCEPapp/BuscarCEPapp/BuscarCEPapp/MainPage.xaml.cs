1d5a5126961bdafcbd71e8b5ee22529ac7757e20
Gabriel Spiandorello <gabriel.spiandorello@brit.com.br>
2018-07-17 22:47:14 -0300

    Projeto Inicial, Buscar Cep funcional - 00

---
+++ BuscarCEPapp/BuscarCEPapp/BuscarCEPapp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using BuscarCEPapp.Servico.Modelo;
+using BuscarCEPapp.Servico;
+
+namespace BuscarCEPapp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BOTAO.Clicked += BuscarCep;
+		}
+            
+        private void BuscarCep(object obj, EventArgs args)
+        {
+            //a fazer LOGICA DO PROGRAMA
+
+            // fazer validações
+
+            string cep = CEP.Text.Trim();
+
+            Endereco end = ViaCepServico.BuscarEnderecoViaCep(cep);
+
+
+            RESULTADO.Text = string.Format("Endereço do cep digitado é: {0}, {1} {2}", end.localidade, end.uf, end.logradouro);
+
+        }
+    }
+}
+ 
---
+++ BuscarCEPapp/BuscarCEPapp/BuscarCEPapp/Servico/ViaCepServico.cs
@@ -0,0 +1,26 @@
+﻿using BuscarCEPapp.Servico.Modelo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace BuscarCEPapp.Servico
+{
+    class ViaCepServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoUrl = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoUrl);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return end;
+        }
+    }
+}
